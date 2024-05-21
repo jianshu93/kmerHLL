99b5ca293297fbec6ff51a3c4cd865a32dfb98e6
jianshu93 <jansuechao@gmail.com>
2024-05-20 23:20:34 -0400

    streaming processing and concurrent programming worked

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 use clap::{Arg, Command};
 use needletail::{parse_fastx_file, Sequence};
+use needletail::kmer::Kmers;
 use rayon::prelude::*;
-use std::error::Error;
 use std::sync::{Arc, Mutex};
+use std::thread;
+use crossbeam_channel::{bounded, unbounded};
 use streaming_algorithms::HyperLogLog;
+use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let matches = Command::new("K-mer Counter")
         .version("0.1.0")
-        .author("Your Name")
-        .about("Counting unique k-mers in sequence files via HLL")
+        .author("Jianshu Zhao")
+        .about("Counting unique k-mers in sequence files via HyperLogLog and HyperMinHash")
         .arg(Arg::new("fasta_file1")
             .help("The first FASTA file to read")
             .required(true)
@@ -50,38 +53,61 @@ fn main() -> Result<(), Box<dyn Error>> {
 fn process_file(file_name: &str, kmer_length: usize) -> Result<HyperLogLog<Vec<u8>>, Box<dyn Error>> {
     let mut reader = parse_fastx_file(file_name)?;
     let hll = Arc::new(Mutex::new(HyperLogLog::<Vec<u8>>::new(0.00408)));
+    let (sender, receiver) = bounded(20);
 
-    let mut sequences = Vec::new();
-    while let Some(result) = reader.next() {
-        let record = result?;
-        sequences.push(record.seq().to_vec());
-    }
-
-    // Determine chunk size based on the number of available CPUs
-    let mut chunk_size = sequences.len() / rayon::current_num_threads();
-    //just incase there is not enough number of sequences to run on multiple threads
-    if chunk_size == 0 {
-        chunk_size += 1;
-    }
-
-    sequences
-        .chunks(chunk_size)
-        .collect::<Vec<_>>()
-        .into_par_iter()
-        .for_each(|chunk| {
-            let mut local_hll = HyperLogLog::<Vec<u8>>::new(0.00408);
-            for seq in chunk {
-                let kmer_length_u8 = kmer_length.try_into().unwrap(); // Handle error appropriately
-
-                for kmer in needletail::kmer::Kmers::new(seq, kmer_length_u8) {
-                    local_hll.push(&kmer.to_vec());
+    // Spawn a thread to read sequences and batch them
+    let reader_thread = thread::spawn(move || {
+        let mut batch = Vec::new();
+        while let Some(result) = reader.next() {
+            if let Ok(record) = result {
+                batch.push(record.seq().to_vec());
+                if batch.len() == 40000 {
+                    sender.send(batch.clone()).expect("Failed to send batch");
+                    batch = Vec::new(); // Reset the batch
                 }
             }
+        }
+        if !batch.is_empty() {
+            sender.send(batch).expect("Failed to send the final batch");
+        }
+    });
 
-            let mut global_hll = hll.lock().unwrap(); // Lock the global HyperLogLog for update
-            global_hll.union(&local_hll); // Merge local HyperLogLog into global one
-        });
+    // Process batches of sequences and update the global hll once per batch
+    let processor_thread = thread::spawn({
+        let hll_clone = Arc::clone(&hll);
+        move || {
+            receiver.into_iter().for_each(|batch| {
+                // Use fold to create a local HyperLogLog for each thread
+                let local_hll = batch.par_iter().fold(
+                    || HyperLogLog::<Vec<u8>>::new(0.00408),
+                    |mut acc, seq| {
+                        let kmer_length_u8 = kmer_length.try_into().unwrap();
+                        for kmer in Kmers::new(seq, kmer_length_u8) {
+                            acc.push(&kmer.to_vec());
+                        }
+                        acc
+                    }
+                );
 
-    let final_hll = Arc::try_unwrap(hll).expect("Lock still has multiple owners");
-    final_hll.into_inner().map_err(Into::into)
+                // Use reduce to merge all local HyperLogLogs into one
+                let reduced_hll = local_hll.reduce(
+                    || HyperLogLog::<Vec<u8>>::new(0.00408),
+                    |mut a, b| {
+                        a.union(&b);
+                        a
+                    }
+                );
+
+                let mut global_hll = hll_clone.lock().unwrap();
+                global_hll.union(&reduced_hll);  // Perform the union operation once per batch
+            });
+        }
+    });
+
+    reader_thread.join().unwrap();
+    processor_thread.join().unwrap();
+
+    Arc::try_unwrap(hll)
+        .map_err(|_arc| Box::new(std::io::Error::new(std::io::ErrorKind::Other, "Lock still has multiple owners")) as Box<dyn Error>)
+        .and_then(|mutex| mutex.into_inner().map_err(|e| Box::new(e) as Box<dyn Error>))
 }
